79b084eed0d7a22761e0035645c2b9f3a8ee4016
Scott Felch <sfelch@gmail.com>
2017-06-12 21:30:08 -0400

    Initial commit

---
+++ mcl_backup.py
@@ -0,0 +1,22 @@
+## THIS FILE IS COPYRIGHTED
+##  PRATT & WHITNEY 2017 
+## FOR INTERNAL USE ONLY 
+## MAY CONTAIN TECHNICAL DATA
+#
+# Author: Scott Felch
+# Build date: 6/12/17
+# Last updated: 6/6/26
+#
+# Note: If the location or name of files changes on the 
+#       network, that will disrupt this script. File paths
+#       are hard coded.
+#
+# TODO: Handle file not found exception
+
+# open input file -- READ mode
+# open output file -- CREATE mode
+
+# perform file transfer
+# close files
+# eject USB dick 
+
