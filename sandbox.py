a5f8b622032af4bfb971a741c22f5cb3206d44ce
Scott Felch <sfelch@gmail.com>
2017-07-10 21:42:06 -0400

    Figured out how md5 works in sandbox. Will implement in prod later. Food time.

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,32 @@
 import hashlib
- 
-hasher = hashlib.md5()
-with open('trump.gif', 'rb') as afile:
-    buf = afile.read()
-    hasher.update(buf)
-print(hasher.hexdigest())
+
+md5_hasher = hashlib.md5()
+sha1_hasher = hashlib.sha1()
+
+with open('trump.webp', 'rb') as md5_file:
+    buf = md5_file.read()
+    md5_hasher.update(buf)
+
+with open('trump.webp', 'rb') as sha1_file:
+    buf = sha1_file.read()
+    sha1_hasher.update(buf)
+
+print('\ntrump.webp')
+print('md5:\t', md5_hasher.hexdigest())
+print('sha1:\t', sha1_hasher.hexdigest())
+
+md5_hasher2 = hashlib.md5()
+sha1_hasher2 = hashlib.sha1()
+
+with open('trump2.webp', 'rb') as md5_file:
+    buf = md5_file.read()
+    md5_hasher2.update(buf)
+
+with open('trump2.webp', 'rb') as sha1_file:
+    buf = sha1_file.read()
+    sha1_hasher2.update(buf)
+
+print('\ntrump2.webp')
+print('md5:\t', md5_hasher2.hexdigest())
+print('sha1:\t', sha1_hasher2.hexdigest())
+
